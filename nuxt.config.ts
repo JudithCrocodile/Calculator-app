dbba6e54f063fb6d26389de9ca009079625c0bde
Judith Lee <argentum1218@gmail.com>
2024-06-27 08:31:56 -0400

    add lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    // Simple usage
+    '@nuxtjs/stylelint-module',
+    '@nuxt/eslint',
+
+    // With options
+    [
+      '@nuxtjs/stylelint-module',
+      {
+        /* module options */
+      }
+    ]
+  ],
+
+  eslint: {
+    config: {
+      stylistic: true // <---
+    }
+  }
 })
