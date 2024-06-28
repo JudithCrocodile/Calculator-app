874e65cb912c7c0618b640911a255db8dcf0bd2f
Judith Lee <argentum1218@gmail.com>
2024-06-28 10:22:35 -0400

    add calculator methods

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,67 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const val1 = ref<string>('0')
+const val2 = ref<string | null>(null)
+const currentSign = ref<string>('')
+
+const calc = function (): void {
+  let result: number = 0
+  if (val2.value === null) {
+    return
+  }
+  switch (currentSign.value as any) {
+    case '+': // addition
+      result = Number(val1.value) + Number(val2.value)
+      break
+    case '-': // subtraction
+      result = Number(val1.value) - Number(val2.value)
+      break
+    case 'x': // multiplication
+      result = Number(val1.value) * Number(val2.value)
+      break
+    case '/': // division
+      result = Number(val1.value) / Number(val2.value)
+      break
+    default:
+  }
+  val1.value = String(Number(parseFloat(String(result)).toPrecision(12)))
+  val2.value = null
+  currentSign.value = ''
+}
+
+const reset = function (): void {
+  val1.value = '0'
+  val2.value = null
+}
+
+const del = function (): void {
+  if (val2.value !== null) {
+    val2.value = val2.value.slice(0, val2.value.length - 1)
+    return
+  }
+
+  val1.value = val1.value.slice(0, val1.value.length - 1)
+}
+
+const keyClick = function (element: any): void {
+  const key = element.currentTarget.getAttribute('data-key')
+  if (currentSign.value !== '') {
+    val2.value = val2.value || ''
+    val2.value += key
+    return
+  }
+
+  val1.value = val1.value === '0' ? key : (val1.value += key)
+}
+
+const signClick = function (element: any): void {
+  const key = element.currentTarget.getAttribute('data-key')
+  currentSign.value = key
+
+  calc()
+}
+</script>
+
 <template>
   <div class="calculator">
     <div class="calculator__container">
@@ -22,66 +86,142 @@
         </div>
       </div>
       <div class="calculator__input-container">
-        <div class="calculator__input">399,981</div>
+        <div class="calculator__input">
+          <p v-if="val2 === null">
+            {{ val1 }}
+          </p>
+          <p v-else>
+            {{ val2 }}
+          </p>
+        </div>
       </div>
       <div
         class="calculator__keys-container grid grid-cols-4 grid-rows-5 gap-3 md:gap-2"
       >
-        <button class="key-1 calculator__key order-9" data-key="1">
+        <button
+          class="key-1 calculator__key order-9"
+          data-key="1"
+          @click="keyClick($event)"
+        >
           <span>1</span>
         </button>
-        <button class="key-1 calculator__key order-10" data-key="2">
+        <button
+          class="key-1 calculator__key order-10"
+          data-key="2"
+          @click="keyClick($event)"
+        >
           <span>2</span>
         </button>
-        <button class="key-1 calculator__key order-11" data-key="3">
+        <button
+          class="key-1 calculator__key order-11"
+          data-key="3"
+          @click="keyClick($event)"
+        >
           <span>3</span>
         </button>
-        <button class="key-1 calculator__key order-5" data-key="4">
+        <button
+          class="key-1 calculator__key order-5"
+          data-key="4"
+          @click="keyClick($event)"
+        >
           <span>4</span>
         </button>
-        <button class="key-1 calculator__key order-6" data-key="5">
+        <button
+          class="key-1 calculator__key order-6"
+          data-key="5"
+          @click="keyClick($event)"
+        >
           <span>5</span>
         </button>
-        <button class="key-1 calculator__key order-7" data-key="6">
+        <button
+          class="key-1 calculator__key order-7"
+          data-key="6"
+          @click="keyClick($event)"
+        >
           <span>6</span>
         </button>
-        <button class="key-1 calculator__key order-1" data-key="7">
+        <button
+          class="key-1 calculator__key order-1"
+          data-key="7"
+          @click="keyClick($event)"
+        >
           <span>7</span>
         </button>
-        <button class="key-1 calculator__key order-2" data-key="8">
+        <button
+          class="key-1 calculator__key order-2"
+          data-key="8"
+          @click="keyClick($event)"
+        >
           <span>8</span>
         </button>
-        <button class="key-1 calculator__key order-3" data-key="9">
+        <button
+          class="key-1 calculator__key order-3"
+          data-key="9"
+          @click="keyClick($event)"
+        >
           <span>9</span>
         </button>
-        <button class="key-1 calculator__key order-13" data-key=".">
+        <button
+          class="key-1 calculator__key order-13"
+          data-key="."
+          @click="keyClick($event)"
+        >
           <span>.</span>
         </button>
-        <button class="key-1 calculator__key order-14" data-key="0">
+        <button
+          class="key-1 calculator__key order-14"
+          data-key="0"
+          @click="keyClick($event)"
+        >
           <span>0</span>
         </button>
-        <button class="key-1 calculator__key order-15" data-key="/">
+        <button
+          class="key-1 calculator__key order-15"
+          data-key="/"
+          @click="signClick($event)"
+        >
           <span>/</span>
         </button>
-        <button class="key-2 calculator__key order-4" data-key="del">
+        <button
+          class="key-2 calculator__key order-4"
+          data-key="del"
+          @click="del()"
+        >
           <span>DEL</span>
         </button>
-        <button class="key-1 calculator__key order-8" data-key="+">
+        <button
+          class="key-1 calculator__key order-8"
+          data-key="+"
+          @click="signClick($event)"
+        >
           <span>+</span>
         </button>
-        <button class="key-1 calculator__key order-12" data-key="-">
+        <button
+          class="key-1 calculator__key order-12"
+          data-key="-"
+          @click="signClick($event)"
+        >
           <span>-</span>
         </button>
-        <button class="key-1 calculator__key order-16" data-key="x">
+        <button
+          class="key-1 calculator__key order-16"
+          data-key="x"
+          @click="signClick($event)"
+        >
           <span>x</span>
         </button>
         <button
           class="key-2 calculator__key order-17 col-span-2"
           data-key="RESET"
+          @click="reset"
         >
           <span>RESET</span>
         </button>
-        <button class="key-3 calculator__key order-18 col-span-2" data-key="=">
+        <button
+          @click="calc()"
+          class="key-3 calculator__key order-18 col-span-2"
+          data-key="="
+        >
           <span>=</span>
         </button>
       </div>
@@ -124,6 +264,14 @@ $radius: 0.3rem;
     border-radius: $radius;
     text-align: right;
     font-size: 1.5rem;
+  }
+
+  &__input {
+    height: 1em;
+
+    p {
+      overflow-x: auto;
+    }
   }
 
   &__keys-container {
